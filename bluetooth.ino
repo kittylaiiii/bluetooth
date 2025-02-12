ed04ab38c93c734e91354404f31e44b5e0a61e58
Kitty Lai <kittylai@mac.milton.edu>
2025-02-12 11:24:33 -0500

    bluetooth

---
+++ bluetooth.ino
@@ -0,0 +1,17 @@
+int const RX_PIN = 3 //receiving bluetooth
+int const TX_PIN = 4 //transmitting bluetooth
+
+#include <SoftwareSerial.h> //include library
+
+SoftwareSerial tooth(TX_PIN, RX_PIN); //create a softwareserial object, set tx and rx pins, tx goes first then rx
+
+
+void setup() {
+
+
+}
+
+void loop() {
+
+
+}
