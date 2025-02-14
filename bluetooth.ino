bf7bc29412d9949e0f0fe32e9765b8b39cc3f4a5
Kitty Lai <kittylai@mac.milton.edu>
2025-02-14 13:53:07 -0500

    add to code

--- bluetooth.ino
+++ bluetooth.ino
@@ -1,17 +1,31 @@
-int const RX_PIN = 3 //receiving bluetooth
-int const TX_PIN = 4 //transmitting bluetooth
+int const RX_PIN = 3; //receiving bluetooth
+int const TX_PIN = 4; //transmitting bluetooth
 
 #include <SoftwareSerial.h> //include library
 
 SoftwareSerial tooth(TX_PIN, RX_PIN); //create a softwareserial object, set tx and rx pins, tx goes first then rx
 
+char a ; //an empty character to store 
+
 
 void setup() {
+
+  Serial.begin(9600); // means can print on serial monitor
+  tooth.begin(9600); //can print on device
 
 
 }
 
 void loop() {
+  if (tooth.available()>0){ //if something is inputed on device
+  a = tooth.read();
+  tooth.print("reading new input: ");
+  tooth.println(a);
+  }
 
-
+  if (a == 'd'){
+    Serial.println("a has value!");
+    
+  }
+delay(100);
 }
